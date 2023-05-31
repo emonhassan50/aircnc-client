aaa41b402bf43146341ba75c0dfc9987af9b051c
Emon <emonhasan7650@gmail.com>
2023-05-31 21:25:51 +0600

    build aircnc part -1 done

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,32 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Main from '../layouts/Main'
+import Home from '../components/pages/Home/Home'
+import Login from '../components/pages/Login/Login'
+import SignUp from '../components/pages/SignUp/SignUp'
+import RoomDetails from '../components/pages/RoomDetails/RoomDetails'
+import PrivateRoute from './PrivateRoute'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Main />,
+    children: [
+      {
+        path: '/',
+        element: <Home/>,
+      },
+      {
+        path: '/room/:id',
+        element: <PrivateRoute><RoomDetails/></PrivateRoute>
+      },
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
   },
 ])
